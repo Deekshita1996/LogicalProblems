2fb9bd982353f12c04dfc3b9b75006027ec047b7
Deekshita1996 <113499307+Deekshita1996@users.noreply.github.com>
2022-10-17 19:58:51 +0530

    Add files via upload

---
+++ PerfectNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class PerfectNumber
+    {
+        public static void PerfectNo()
+        {
+            int number, sum = 0; int n;
+            Console.Write("Enter the number");
+            number = int.Parse(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
