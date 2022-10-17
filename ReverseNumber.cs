46311ed414a25251fa1a8b5c5dbea239813586f2
Deekshita1996 <113499307+Deekshita1996@users.noreply.github.com>
2022-10-17 20:10:59 +0530

    Add files via upload

---
+++ ReverseNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class ReverseNumber
+    {
+        public static void GetNumber()
+        {
+            int n, rev = 0;
+            Console.WriteLine("Enter any number: ");
+            n = int.Parse(Console.ReadLine());
+            int reverse = 0;
+           
+            while (n > 0)
+            {
+                var remainder = n % 10;
+                 reverse = (reverse * 10) + remainder;
+                n = n / 10;
+            }
+            Console.WriteLine("Reverse No. is {0}", reverse);
+
+            
+        }
+    }
+}
+  
